a062c0040df32be3b4966c0cd70e84b1d7763db8
Markonav <manavarro24@alumnos.utalca.cl>
2025-10-02 19:39:48 -0300

    feat: añadir ruta para categorias

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,7 @@ import EditarPerfil from '@/views/EditarPerfil.vue'
 import Configuracion from '@/views/Configuracion.vue'
 import Favoritos from '@/views/Favoritos.vue'
 import AgregarLibro from '@/views/AgregarLibro.vue'
+import Categorias from '@/views/Categorias.vue'
 
 
 const router = createRouter({
@@ -16,6 +17,7 @@ const router = createRouter({
     { path: '/', redirect: '/menu' },
     { path: '/menu', component: Menu },
     { path: '/catalogo', component: Catalogo },
+    { path: '/categorias', component: Categorias },
     { path: '/login', component: Login },
     { path: '/agregarLibro', component: AgregarLibro },
     {
