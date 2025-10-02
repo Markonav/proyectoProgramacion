b14394c1c092414a2f41fa8f54755bce8deb52bc
Markonav <manavarro24@alumnos.utalca.cl>
2025-10-02 17:53:13 -0300

    feat: crear vista de la pagina de cuenta

---
+++ frontend/src/views/Cuenta.vue
@@ -0,0 +1,30 @@
+<template>
+  <Header />
+  <div class="perfil cuenta-layout">
+    <div class="perfil__grid">
+      <SideBarPerfil />
+      <router-view />
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header.vue';
+import SideBarPerfil from '@/components/SideBarCuenta.vue';
+export default {
+  name: 'Cuenta',
+  components: { Header, Footer, SideBarPerfil }
+}
+</script>
+
+<style src="@/assets/styles/cuentaConfiguracion.css"></style>
+<style>
+.cuenta-layout {
+  min-height: calc(100vh - 120px); /* Ajusta 120px según el alto de tu header+footer */
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+</style>
